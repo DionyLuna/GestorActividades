30225d8b8b90b36811f6917ad07cf7c8c73885ca
sluna <santosluna0705@gmail.coms>
2024-12-11 14:58:38 -0400

    Update01

---
+++ Modelo/Datos/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public abstract class Conexion
+    {
+        private readonly string ConexionString;
+
+        public Conexion()
+        {
+            ConexionString = "Server=(local)\\L-ERAMIREZ; database=ActividadesDB; integrated segurity=true";
+        }
+        protected SqlConnection GetConexion()
+        {
+            return new SqlConnection(ConexionString);
+        }
+
+    }
+}
---
+++ Modelo/Entidades/clsCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clsCache
+    {
+        public static int UserID { get; set; }
+        public static string UserName { get; set; }=string.Empty;
+        public static string UserEmail { get; set;} = string.Empty;
+    }
+}
